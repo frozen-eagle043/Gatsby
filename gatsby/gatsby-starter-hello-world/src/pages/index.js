79f3f663f80650e32b4668920f463d58483d54f4
Shivam Devaser <shivamdevaser934@gmail.com>
2022-01-07 05:28:40 +0000

    Minor style Changes

--- gatsby/gatsby-starter-hello-world/src/pages/index.js
+++ gatsby/gatsby-starter-hello-world/src/pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
           <h3>Develop & Deploy</h3>
           <p>UX designer & web developer based in India.</p>
           <Link className={style.btn} to="/projects">My Portfolio Projects</Link>
+          <Link className={style.btn} to="/blank">Blank</Link>
         </div>
       </section>
     </Layout>
